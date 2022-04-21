590d19743f487a4ecfed5aef8133b3abcab92945
ezanolli <97760201+ezanolli@users.noreply.github.com>
2022-04-21 18:39:00 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -10,7 +10,7 @@ var radarDisplayOptions = {
 var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
 var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
 $.getJSON(weatherAlertsUrl, function(data) {
-  L.getJSON(data, {
+  L.geoJSON(data, {
   style: function(feature){
   var alertColor = 'orange';
   if (feature.properties.severity === 'Severe') alertColor = 'red';
